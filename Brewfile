e98c5cee54966a97b88c3efab0d7ddf4bfd98fd0
Quinn Winters <email@quinnwinters.dev>
2020-04-07 00:07:09 -0400

    settingup dotfiles to work fully

---
+++ Brewfile
@@ -0,0 +1,82 @@
+# Taps which should be considered core
+tap 'homebrew/cask'
+tap 'homebrew/cask'
+tap 'homebrew/cask-fonts'
+tap 'homebrew/cask-versions'
+tap 'homebrew/bundle'
+tap 'homebrew/cask'
+
+# Core bits
+brew 'bash'
+brew 'coreutils'
+brew 'git'
+brew 'grep'
+brew 'mas'
+brew 'tree'
+
+# Core apps
+cask 'google-chrome'
+cask 'firefox'
+cask 'spotify'
+cask 'alfred'
+
+# Core fonts
+cask 'font-roboto'
+cask 'font-roboto-condensed'
+cask 'font-roboto-code-pro-for-powerline'
+cask 'font-roboto-code-pro'
+cask 'font-roboto-sans-pro'
+cask 'font-roboto-serif-pro'
+
+# Languages and programming tools
+brew 'python'
+brew 'python3'
+brew 'scala'
+brew 'sbt'
+brew 'ocaml'
+brew 'opam'
+brew 'r'
+cask 'java'
+cask 'julia'
+
+# Programming utils
+cask 'pyenv'
+cask 'pyenv-virtualenv'
+cask 'virtualenv'
+cask 'anaconda'
+brew 'gdal'
+brew 'opencv'
+
+# Command line utils
+brew 'awscli'
+brew 'mysql'
+brew 'node'
+brew 'yarn'
+brew 'nginx'
+brew 'moreutils'
+brew 'findutils'
+brew 'wget'
+brew 'curl'
+brew 'the_silver_searcher'
+brew 'jq'
+brew 'reattach-to-user-namespace'
+brew 'tmux'
+brew 'ack'
+brew 'zsh'
+brew 'htop'
+
+# More professional focused apps (e.g. IDEs)
+cask 'intellij-idea-ce'
+cask 'pycharm-idea-ce'
+cask 'rstudio'
+cask 'iterm2'
+cask 'virtualbox'
+cask 'mactex'
+# Personal apps
+cask 'whatsapp'
+cask '1password'
+cask 'caffeine'
+cask 'flux'
+cask 'vlc'
+cask 'google-backup-and-sync'
+cask 'google-drive'
--- dotfiles.sh
+++ dotfiles.sh
@@ -2,6 +2,109 @@
 
 set -eu
 
+COMMAND=$1
+
+# Installation flags
+INSTALL_CORE=false
+INSTALL_WORK=false
+INSTALL_PERS=false
+INSTALL_ALL=false
+
+# configuration falgs
+CONFIGURE_ALL=false
+CONFIGURE_ZSH=false
+CONFIGURE_TMUX=false
+CONFIGURE_GIT=false
+CONFIGURE_MAC=false
+CONFIGURE_VIM=false
+CONFIGURE_ITERM=false
+CONFIGURE_FIREFOX=false
+CONFIGURE_DRIVE=false\
+
+print_help() {
+  if [ "$1"=="install" ]; then 
+    echo "Usage for dotfiles install script:"
+    echo "------------------------------------"
+    echo ""
+    echo "\t ./dotfiles.sh install [-c -w -p -a]"
+    echo ""
+    echo "\t\t -c --core \t\t Install core applications only"
+    echo "\t\t -w --work \t\t Install work applications only"
+    echo "\t\t -p --personal \t Install personal applications only"
+    echo "\t\t -a --all \t\t Install all applications and run configurations"
+    echo ""
+    echo ""
+    exit 0
+  elif [ "$1"=="configure" ]; then
+    echo "Usage for dotfiles configure script:"
+    echo "------------------------------------"
+    echo ""
+    echo "\t ./dotfiles.sh configure [-z -t -g -m -v -i -f -d -a]"
+    echo ""
+    echo "\t\t -z --zsh \t\t\t Configure zsh and oh-my-zsh"
+    echo "\t\t -t --tmux \t\t\t Configure tmux"
+    echo "\t\t -g --git \t\t\t Configure gitignore and gitconfig"
+    echo "\t\t -m --mac \t\t\t Configure macos core settings"
+    echo "\t\t -v --vim \t\t\t Configure vim and various vim plugins"
+    echo "\t\t -i --iterm \t\t\t Configure iTerm2 themes and settings"
+    echo "\t\t -f --firefox \t\t Configure Firefox bookmarks and plugins"
+    echo "\t\t -d --google-drive\t Configure Google Drive file mount"
+    echo "\t\t -a --all \t\t Configure all settings"
+    exit 0
+  else 
+    echo "Please use ./dotfiles.sh install --help or ./dotfiles.sh configure --help for usage"
+    echo 0
+  fi
+}
+
+while test $
+do
+    case "$1" in 
+    -c|--core)
+      INSTALL_CORE=true
+      ;;
+    -w|--work)
+      INSTALL_WORK=true
+      ;;
+    -p|--personal)
+      INSTALL_PERSONAL=true
+    -a|--all)
+      INSTALL_ALL=true
+      CONFIGURE_ALL=true
+      ;;
+    -z|--zsh)
+      CONFIGURE_ZSH=true
+      ;;
+    -t|--tmux)
+      CONFIGURE_TMUX=true
+      ;;
+    -g|--git)
+      CONFIGURE_GIT=true
+      ;;
+    -m|--mac)
+      CONFIGURE_MAC=true
+      ;;
+    -v|--vim)
+      CONFIGURE_VIM=true
+      ;;
+    -i|--iterm)
+      CONFIGURE_ITERM=true
+      ;;
+    -f|--firefox)
+      CONFIGURE_FIREFOX=true
+      ;;
+    -d|--google-drive)
+      CONFIGURE_DRIVE=true
+      ;;
+    -h|--help)
+      print_help COMMAND
+    *)
+      break
+      ;;
+  esac
+done
+
+
 # Setup homebrew, update, and tap the homebrew/bundle
 setup_brew() {
   if test ! $(which brew); then 
@@ -11,21 +114,27 @@ setup_brew() {
   
   brew update
   brew tap homebrew/bundle
+
+  touch Brewfile
 }
 
 # copy core brew instructions into Brewfile
 core_brew() {
-  cat .homebrew/core > Brewfile
+  cat Brewfile .homebrew/core > Brewfile
 }
 
 # copy work brew instructions into Brewfile
 work_brew() {
-  cat .homebrew/core ./homebrew/work > Brewfile
+  cat Brewfile .homebrew/work > Brewfile
+}
+
+personal_brew() {
+  cat Brewfile .homebrew/personal > Brewfile
 }
 
 # copy personal brew instructions into Brewfile
 all_brew() {
-  cat .homebrew/core .homebrew/work ./homebrew/personal > Brewfile 
+  cat .homebrew/core .homebrew/work .homebrew/personal > Brewfile 
 }
 
 # Verify the Brewfile file and then run brew and delete the Brewfile
@@ -34,9 +143,10 @@ brew_bundle() {
   brew bundle check 
   brew bundle 
   brew cleanup
+  rm -f Brewfile
 }
 
-setup() {
+configure() {
   echo "Linking files and importing proper configs"
   echo "Finished setting up all the configs"
 }
@@ -46,23 +156,32 @@ install() {
   
   # setting up homebrew properly 
   setup_brew 
-  
   # creating a local Brewfile 
-  core_brew
-  work_brew
-  personal_brew
+  if $CONFIGURE_ALL ; then 
+    all_brew
+  else 
+    if $CONFIGURE_CORE ; then 
+      core_brew
+    elif $CONFIGURE_WORK ; then 
+      work_brew
+    elif $PERSONAL_BREW ; 
+      personal_brew
+    fi
+  fi 
   
   # actually run the brew 
   brew_bundle 
 
   # Use the setup function to configure things appropriately
-  setup
+  if $CONFIGURE_ALL ; then 
+    configure
+  fi 
   echo "Finished installing software and running proper setups"
 }
 
 if declare -f "$1" > /dev/null
 then 
-  "$@"
+  "$1"
 else 
   echo "$1 is not a supported operation. Please use 'install' or 'setup'" >&2
   exit 1
