d89bdaf1a73d7f9f11a014b8a1d8624460f51736
Restyled.io <commits@restyled.io>
2020-05-04 14:00:00 +0000

    Restyled by shellharden

--- git/_setup.sh
+++ git/_setup.sh
@@ -6,21 +6,21 @@ ln -sf ./gitignore ~/.gitignore
 REPODIR=~/Documents/GitHub
 if [[ ! -d $REPODIR ]] ; then
     echo "Making directory for all git projects: $REPODIR"
-    mkdir -vp $REPODIR
+    mkdir -vp "$REPODIR"
 fi
 
-PWD=$(pwd)
-cd $REPODIR
+PWD=$PWD
+cd "$REPODIR"
 curl -k "https://api.github.com/users/quinnwinters/repos?page=1&per_page=100" | \
     grep -e 'git_url*' | \
     grep -vwE 'dotfiles' | \
     cut -d \" -f 4 | \
     xargs -L1 git clone
-cd $PWD
+cd "$PWD"
 
 if [[ ! -d ~/.aliases ]] ; then
   echo "Creating alias directory at ~/.aliases"
   mkdir -vp ~/.aliases
 fi
 
-ln -sf $(pwd)/gitaliases ~/.aliases/gitaliases
+ln -sf "$PWD"/gitaliases ~/.aliases/gitaliases
