7cc245899c03c362f9530f2519ef7e0bbb35380d
Quinn Winters <quinnwinters@Quinns-MBP.fios-router.home>
2020-05-04 03:18:53 -0400

    verified

--- cron/_setup.sh
+++ cron/_setup.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 CDIR=$(pwd)
-CRONLOC=/var/spool/cron/quinnwinters
-touch $CRONLOC
+
+if [[ ! -d "~/.cron" ]] ; then 
+    mkdir -vp ~/.cron 
+fi
+
+CRONLOC=~/.cron/jobs
+crontab -l > $CRONLOC
 
 echo "30 8 * * * sh $CDIR/email_me_todos.sh" >> $CRONLOC
 echo "0 16 * * 5 sh $CDIR/email_me_accomplishments" >> $CRONLOC
 echo "0 0 * * 0 sh $CDIR/update_homebrew.sh" >> $CRONLOC
- 
+
+
+cat $CRONLOC | uniq > $CRONLOC
+cron $CRONLOC
