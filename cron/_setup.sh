6573ceaac305ebfc9873638afd74ca5c4f21e5a1
Quinn Winters <3493504+quinnwinters@users.noreply.github.com>
2020-05-10 23:49:24 -0400

    Tmux + Cron job scripts (#15)
    * cron scripts fixed up after
    
    * Restyle Tmux + Cron job scripts (#16)
    
    * Restyled by jq
    
    * Restyled by prettier-json
    
    * Restyled by prettier-markdown
    
    * Restyled by shellharden
    
    * Restyled by shfmt
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    Co-authored-by: Quinn Winters <>
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

--- cron/_setup.sh
+++ cron/_setup.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-CDIR=$(pwd)
+CDIR=$PWD
 
-if [[ ! -d "~/.cron" ]] ; then 
-    mkdir -vp ~/.cron 
+if [[ ! -d "~/.cron" ]]; then
+  mkdir -vp ~/.cron
 fi
 
 CRONLOC=~/.cron/jobs
-crontab -l > $CRONLOC
+crontab -l >"$CRONLOC"
 
-echo "30 8 * * * sh $CDIR/email_me_todos.sh" >> $CRONLOC
-echo "0 16 * * 5 sh $CDIR/email_me_accomplishments" >> $CRONLOC
-echo "0 0 * * 0 sh $CDIR/update_homebrew.sh" >> $CRONLOC
+echo "30 8 * * * sh $DOT_DIR/cron/scripts/email_me_todos.sh" >>"$CRONLOC"
+echo "0 16 * * 5 sh $DOT_DIR/cron/scripts/email_me_accomplishments" >>"$CRONLOC"
+echo "0 0 * * 0 sh $DOT_DIR/cron/scripts/update_homebrew.sh" >>"$CRONLOC"
 
+echo "Setting up cron file using the following configuration:"
+cat "$CRONLOC"
 
-cat $CRONLOC | uniq | crontab -
+cat "$CRONLOC" | uniq | crontab -
