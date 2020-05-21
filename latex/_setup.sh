07a4eca6d7c013b0d8e5f271df58efde8ba85bbf
Quinn Winters <3493504+quinnwinters@users.noreply.github.com>
2020-05-20 23:28:49 -0400

    Updating LaTeX template (#17)
    * adding updates to latex template based on current usage
    
    * Restyle Updating LaTeX template (#18)
    
    * Restyled by shellharden
    
    * Restyled by shfmt
    
    * Restyled by whitespace
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    Co-authored-by: Quinn Winters <>
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

--- latex/_setup.sh
+++ latex/_setup.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 texversion=$(latex --version 2>&1)
-if [[ $? != 0 ]] ; then
-    echo "Installing latex"
-    brew cask install mactex texstudio
+if [[ $? != 0 ]]; then
+  echo "Installing latex"
+  brew cask install mactex texstudio
 fi
 
 TEXTEMPLATES=~/.latex_templates
-if [[ ! -d $TEXTEMPLATES ]] ; then 
-    echo "Making LaTeX templates collection: $TEXTEMPLATES"
-    mkdir -vp $TEXTEMPLATES
+if [[ ! -d $TEXTEMPLATES ]]; then
+  echo "Making LaTeX templates collection: $TEXTEMPLATES"
+  mkdir -vp "$TEXTEMPLATES"
 fi
 
-ln -sf $(pwd)/mathematics-header.tex ~/.latex_templates/mathematics-header.tex
-ln -sf $(pwd)/texaliases ~/.aliases/texaliases
-
+ln -sf "$PWD"/mathematics-template.tex ~/.latex_templates/mathematics-template.tex
+ln -sf "$PWD"/texaliases ~/.aliases/texaliases
