7141ac89e6091427eadd941fd722b90ba0d88233
Restyled.io <commits@restyled.io>
2020-05-21 03:27:41 +0000

    Restyled by shfmt

--- latex/_setup.sh
+++ latex/_setup.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 texversion=$(latex --version 2>&1)
-if [[ $? != 0 ]] ; then
-    echo "Installing latex"
-    brew cask install mactex texstudio
+if [[ $? != 0 ]]; then
+  echo "Installing latex"
+  brew cask install mactex texstudio
 fi
 
 TEXTEMPLATES=~/.latex_templates
-if [[ ! -d $TEXTEMPLATES ]] ; then
-    echo "Making LaTeX templates collection: $TEXTEMPLATES"
-    mkdir -vp "$TEXTEMPLATES"
+if [[ ! -d $TEXTEMPLATES ]]; then
+  echo "Making LaTeX templates collection: $TEXTEMPLATES"
+  mkdir -vp "$TEXTEMPLATES"
 fi
-
 
 ln -sf "$PWD"/mathematics-template.tex ~/.latex_templates/mathematics-template.tex
 ln -sf "$PWD"/texaliases ~/.aliases/texaliases
-
