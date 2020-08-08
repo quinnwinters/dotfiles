b7aedc0aef76c1d99bafaad8cfd56fae2e3c3f52
Restyled.io <commits@restyled.io>
2020-08-08 23:50:06 +0000

    Restyled by shellharden

--- scripts/mackup-restore.sh
+++ scripts/mackup-restore.sh
@@ -2,7 +2,7 @@
 
 if [[ ! -L "~/.mackup.cfg" ]]; then
     rm -f ~/.mackup.cfg
-    ln -sf $DOT_DIR/home/.mackup.cfg ~/.mackup.cfg
+    ln -sf "$DOT_DIR"/home/.mackup.cfg ~/.mackup.cfg
 fi
 
 mackup restore
