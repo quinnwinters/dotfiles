5617bffa44cdd55684dd6b3faa39c769dd04283f
Quinn Winters <>
2020-08-08 14:56:52 -0700

    adding some automation scripts

---
+++ home/.automation/new-shell-session.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "You have the following tmux sessions open"
+echo "-----------------------------------------"
+tmux ls
+echo ""
+echo "Since you are about to program, you should create a session or attach to"
+echo "an existing session. Type the name of the session to attach or create"
+echo "(Suggestions: {project}-{branch})"
+read -r session
+
+tmux new-session -A -s "$session"
