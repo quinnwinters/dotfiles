fbace5e7a3c007f8417f1d51673f03b6739f90b5
Quinn Winters <email@quinnwinters.dev>
2020-08-31 00:16:21 -0400

    feat: Improvements to tmux bindings, alias, and launch script (#71)
    Co-authored-by: Quinn Winters <>

--- home/.automation/new-shell-session.sh
+++ home/.automation/new-shell-session.sh
@@ -7,6 +7,25 @@ echo ""
 echo "Since you are about to program, you should create a session or attach to"
 echo "an existing session. Type the name of the session to attach or create"
 echo "(Suggestions: {project}-{branch})"
-read -r session
+read -r name
 
-tmux new-session -A -s "$session"
+function HELPER_tmux_load_session() {
+    session="$@"
+
+    if [ ! $(tmux has-session -t "$session") ] ; then
+        echo "Creating tmux session session=$session"
+        tmux new -s "$session" -d
+    fi
+
+    if [ ! $TMUX ] ; then
+        echo "No current tmux session, attaching to session=$session"
+        tmux attach -t "$session"
+    else
+        echo "Current tmux session is session=$(tmux display-message -p '#S')"
+        echo "Switching to session=$session"
+        tmux switch -t "$session"
+    fi
+}
+
+HELPER_tmux_load_session $name
+
