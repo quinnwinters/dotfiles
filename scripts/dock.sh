57da0a90ad02a18caec8ee25afdf3b26c4d91ee9
Quinn Winters <email@quinnwinters.dev>
2020-09-29 12:38:10 -0400

    Small Dock/App Changes (#97)
    * feat: Adding some more installations
    
    * feat: Rearranged dock
    
    * feat: Adding office suite

--- scripts/dock.sh
+++ scripts/dock.sh
@@ -51,6 +51,7 @@ HELPER_add_blankspaces_to_doc
 HELPER_add_apple_default_app_to_doc "System Preferences"
 HELPER_add_apple_default_app_to_doc "App Store"
 HELPER_add_apple_default_app_to_doc "Dictionary"
+HELPER_add_apple_default_app_to_doc "Contacts"
 HELPER_add_apple_default_app_to_doc "Photo Booth"
 HELPER_add_blankspaces_to_doc
 
@@ -61,20 +62,26 @@ HELPER_add_external_user_app_to_doc "Freedom"
 HELPER_add_external_user_app_to_doc "iStat Menus"
 HELPER_add_blankspaces_to_doc
 
-# Productivity applications
-HELPER_add_apple_default_app_to_doc "Contacts"
-HELPER_add_apple_default_app_to_doc "Preview"
+# Office Apps
+HELPER_add_external_user_app_to_doc "Numbers"
+HELPER_add_external_user_app_to_doc "Keynote"
+HELPER_add_external_user_app_to_doc "Pages"
 HELPER_add_external_user_app_to_doc "Notion"
+HELPER_add_external_user_app_to_doc "Agenda"
+HELPER_add_apple_default_app_to_doc "Preview"
+HELPER_add_blankspaces_to_doc
+
+# Productivity applications
 HELPER_add_external_user_app_to_doc "Things3"
 HELPER_add_external_user_app_to_doc "Google Chrome"
 HELPER_add_external_user_app_to_doc "WhatsApp"
 HELPER_add_apple_default_app_to_doc "Messages"
+HELPER_add_external_user_app_to_doc "Messenger"
 HELPER_add_blankspaces_to_doc
 
 # Coding applications
 HELPER_add_external_user_app_to_doc "Slack"
 HELPER_add_external_user_app_to_doc "Gitter"
-HELPER_add_external_user_app_to_doc "GitKraken"
 HELPER_add_external_user_app_to_doc "texstudio"
 HELPER_add_external_user_app_to_doc "Hyper"
 HELPER_add_external_user_app_to_doc "Docker"
@@ -90,8 +97,9 @@ HELPER_add_external_user_app_to_doc "Discord"
 HELPER_add_blankspaces_to_doc
 
 # Media
-HELPER_add_external_user_app_to_doc "Spotify"
+HELPER_add_external_user_app_to_doc "Youtube Music Desktop App"
 HELPER_add_external_user_app_to_doc "VLC"
+HELPER_add_apple_default_app_to_doc "QuickTime Player"
 HELPER_add_external_user_app_to_doc "iMovie"
 HELPER_add_external_user_app_to_doc "HandBrake"
 HELPER_add_apple_default_app_to_doc "TV"
