e03e24e921f0e82da2316f5e8fcfe84971a3de6f
Quinn Winters <quinnwinters@Quinns-MBP.fios-router.home>
2020-11-22 18:32:16 -0500

    refactor: minimizing dotfiles

--- home/.zshenv
+++
@@ -1,4 +0,0 @@
-export DOCUMENTS=$HOME/Documents
-export VISUAL=nvim
-export EDITOR=$VISUAL
-export GIT_EDITOR=$VISUAL
--- scripts/meta-setup/install-xcode-tools.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "[DOTFILES] Requesting sudo access to run xcode-select install"
-sudo xcode-select install
