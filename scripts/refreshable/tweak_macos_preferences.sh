e72f83b074cf1fef23437cf1751cf073b2a246f4
Quinn Winters <email@quinnwinters.dev>
2020-11-22 21:10:48 -0500

    Minimal dotfiles (#117)
    * refactor: minimizing dotfiles
    
    * style: Restyled by whitespace (#120)
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    Co-authored-by: Quinn Winters <quinnwinters@Quinns-MBP.fios-router.home>
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

---
+++ scripts/refreshable/tweak_macos_preferences.sh
@@ -0,0 +1,78 @@
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+# Ask for the administrator password upfront
+sudo -v
+while true; do
+  sudo -n true
+  sleep 60
+  kill -0 "$$" || exit
+done 2>/dev/null &
+
+# ========================================================= #
+# |     UI/UX Settings                                    | #
+# ========================================================= #
+sudo nvram SystemAudioVolume=" "
+defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 2
+defaults write NSGlobalDomain AppleShowScrollBars -string "Automatic"
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+defaults write NSGlobalDomain NSDisableAutomaticTermination -bool true
+
+# ========================================================= #
+# |     Coding Setting Modifications                      | #
+# ========================================================= #
+defaults write com.apple.helpviewer DevMode -bool true
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+# ========================================================= #
+# |     Basic Aesthetics                                  | #
+# ========================================================= #
+# sudo rm -f /System/Library/CoreServices/DefaultBackground.jpg
+# sudo ln -s $DOT_DIR/media/wallpaper-1.jpg /System/Library/CoreServices/DefaultBackground.jpg
+location=$(realpath "$DOT_DIR"/media/wallpaper-1.jpg)
+command="tell application \"Finder\" to set desktop picture to POSIX file \"$location\""
+osascript -e "$command"
+
+# ========================================================= #
+# |     Interantionalization Settings                     | #
+# ========================================================= #
+defaults write com.apple.menuextra.clock DateFormat -string 'EEE MMM d  H:mm:ss'
+defaults write NSGlobalDomain AppleLanguages -array "en" "ar"
+defaults write NSGlobalDomain AppleLocale -string "en_US@currency=USD"
+defaults write NSGlobalDomain AppleMeasurementUnits -string "Centimeters"
+defaults write NSGlobalDomain AppleMetricUnits -bool true
+sudo defaults write /Library/Preferences/com.apple.loginwindow showInputMenu -bool true
+
+# ========================================================= #
+# |     Sleep and Screen Lock Settings                    | #
+# ========================================================= #
+sudo pmset -a lidwake 1
+sudo pmset -a autorestart 1
+sudo systemsetup -setrestartfreeze on
+sudo pmset -c sleep 0
+sudo pmset -b sleep 5
+sudo pmset -a standbydelay 86400
+sudo systemsetup -setcomputersleep Off >/dev/null
+sudo pmset -a hibernatemode 0
+sudo rm /private/var/vm/sleepimage
+sudo touch /private/var/vm/sleepimage
+sudo chflags uchg /private/var/vm/sleepimage
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# ========================================================= #
+# |    Screenshot settings                                | #
+# ========================================================= #
+mkdir -p "$HOME"/Pictures/screenshots
+defaults write com.apple.screencapture location -string "~/Pictures/screenshots"
+defaults write com.apple.screencapture type -string "png"
+defaults write com.apple.screencapture disable-shadow -bool true
