f8332fc555842e03929bc40e9c6b9fc685ac62cc
Quinn Winters <email@quinnwinters.dev>
2020-12-09 17:18:43 -0500

    Qwinters/path (#121)
    * fix: Adding DOT_DIR to zsh
    
    * feat: adding more things
    
    * feat: Fully reseting these things
    
    * feat: adding commit history alias
    
    * feat: adding git compleition
    
    * feat: adding another fix
    
    * feat: more stuff

---
+++ bootstrap.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import argparse
+import subprocess
+import shlex
+
+class SetupTask:
+    def __init__(self, flag, script_location, default_value=True):
+        self.flag = flag
+        self.script_location = script_location
+        self.default_value = default_value
+        self.command = shlex.split(f"sh ./scripts/{script_location}")
+
+    def add_to_parser(self, parser):
+        helper_description = f"Use this flag to run: $DOT_DIR/scripts{self.script_location} (default: {self.default_value})"
+        parser.add_argument(
+            self.flag,
+            description=helper_description,
+            help=helper_description,
+            type=bool,
+            action="store",
+            default=default_value
+        )
+
+    def run(self):
+        subprocess.check_call(self.command)
+    
+
+TASKS = [
+    SetupTask("shell-tools", "environment/setup-shell-tools", default_value=False),
+    SetupTask("github-ssh", "environment/setup-github-ssh", default_value=False),
+    SetupTask("local-exports", "environment/setup-local-exports"),
+    SetupTask("dotfile-links", "environment/setup-dotfile-links"),
+    SetupTask("brew-apps", "install/install-brew-apps"),
+    SetupTask("brew-casks", "install/install-brew-casks"),
+    SetupTask("mac-apps", "install/install-brew-mas"),
+    SetupTask("clone-repos", "install/install-ghq-repos"),
+    SetupTask("node-tools", "install/install-node-tools"),
+    SetupTask("vim-plugins", "install/install-vim-plugins"),
+    SetupTask("macos-prefs", "preferences/setup-macos-prefs"),
+]
+
+parser = argparse.ArgumentParser(description="qwinters dotfiles install script")
+
+for task in TASKS:
+    task.add_to_parser(parser)
+
+if __name__ == "__main__":
+    args = parser.parse_arguments()
+    for task in TASKS:
+        if args[task.flag]:
+            task.run()
+
+
+
+
