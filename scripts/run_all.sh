2e58af6fa890480bb8bd533a396b518de762f118
Quinn Winters <qwinters@MacBook-Pro.fios-router.home>
2021-01-11 14:26:38 -0500

    fix: scripts directory for run_all.sh

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -3,19 +3,19 @@
 DOTDIR=$(git rev-parse --show-toplevel)
 
 # Setup the environment
-sh "$DOTDIR"/environment/setup-shell-tools
-sh "$DOTDIR"/environment/setup-local-exports
-sh "$DOTDIR"/environment/setup-github-ssh
-sh "$DOTDIR"/environment/setup-onedrive-docs
-sh "$DOTDIR"/environment/setup-dotfile-links
+sh "$DOTDIR"/scripts/environment/setup-shell-tools
+sh "$DOTDIR"/scripts/environment/setup-local-exports
+sh "$DOTDIR"/scripts/environment/setup-github-ssh
+sh "$DOTDIR"/scripts/environment/setup-onedrive-docs
+sh "$DOTDIR"/scripts/environment/setup-dotfile-links
 
 # Install software
-sh "$DOTDIR"/install/install-brew-apps
-sh "$DOTDIR"/install/install-brew-casks
-sh "$DOTDIR"/install/install-brew-mas
-sh "$DOTDIR"/install/install-ghq-repos
-sh "$DOTDIR"/install/install-vim-plugins
+sh "$DOTDIR"/scripts/install/install-brew-apps
+sh "$DOTDIR"/scripts/install/install-brew-casks
+sh "$DOTDIR"/scripts/install/install-brew-mas
+sh "$DOTDIR"/scripts/install/install-ghq-repos
+sh "$DOTDIR"/scripts/install/install-vim-plugins
 
 # Tweak preferences
-sh "$DOTDIR"/preferences/setup-macos-prefs
-sh "$DOTDIR"/preferences/setup-mac-dock
+sh "$DOTDIR"/scripts/preferences/setup-macos-prefs
+sh "$DOTDIR"/scripts/preferences/setup-mac-dock
